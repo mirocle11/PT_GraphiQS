f471584a7a16823f8f7873847336e12c07973385
mirocle11 <miro.bayawa@gmail.com>
2019-10-03 09:19:43 +0800

    #6 commit

---
+++ src/Controllers/framingController.java
@@ -0,0 +1,14 @@
+package Controllers;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+        import java.util.ResourceBundle;
+
+public class framingController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
