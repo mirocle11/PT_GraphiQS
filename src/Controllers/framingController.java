159dbe817f9834311070f0e4e7060ff1299738cb
mirocle11 <miro.bayawa@gmail.com>
2019-10-04 10:41:31 +0800

    #7 commit

--- src/Controllers/framingController.java
+++ src/Controllers/framingController.java
@@ -1,14 +1,51 @@
 package Controllers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
-        import java.util.ResourceBundle;
+import java.util.ResourceBundle;
 
 public class framingController implements Initializable {
 
+    @FXML
+    private AnchorPane rootpane,firstpane;
+    private AnchorPane secondpane;
+
+    @FXML
+    private ImageView first,second;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Views/framing_2.fxml"));
+            secondpane = loader.load();
+            rootpane.getChildren().addAll(secondpane);
+            secondpane.setLayoutY(120);
+            secondpane.setVisible(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void next(ActionEvent actionEvent) {
+        firstpane.setVisible(false);
+        secondpane.setVisible(true);
+
+        first.setImage(new Image("Views/images/1_circle_25px.png"));
+        second.setImage(new Image("Views/images/2_circle_c_25px.png"));
+    }
+
+    public void back(ActionEvent actionEvent) {
+        firstpane.setVisible(true);
+        secondpane.setVisible(false);
+
+        first.setImage(new Image("Views/images/1_circle_c_25px.png"));
+        second.setImage(new Image("Views/images/2_circle_25px.png"));
     }
 
 }
---
+++ src/Controllers/framing_2Controller.java
@@ -0,0 +1,15 @@
+package Controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class framing_2Controller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
