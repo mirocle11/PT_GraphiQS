7bd09829ba1026c118e114d32fd1e7ccf27f1452
John Ebarita <ebarita00@gmail.om>
2019-11-18 13:42:40 +0800

    #50 commit

---
+++ src/Data/historyData.java
@@ -0,0 +1,11 @@
+package Data;
+
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class historyData extends RecursiveTreeObject {
+
+
+
+}
